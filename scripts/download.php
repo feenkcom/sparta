1a78fd0158d95a95e42047692560dc853f8227d0
Aliaksei Syrel <alex@syrel.ch>
2016-08-29 15:57:14 +0200

    php download proxy script

---
+++ scripts/download.php
@@ -0,0 +1,41 @@
+<?php
+
+set_time_limit (24 * 60 * 60);
+
+$url = $_GET['url'];
+$newfname = "download_" . microtime() . '_file_' . rand() . "_tmp";
+
+$file = fopen ($url, "rb");
+if ($file) {
+  $newf = fopen ($newfname, "wb");
+
+  if ($newf)
+  while(!feof($file)) {
+    fwrite($newf, fread($file, 1024 * 8 ), 1024 * 8 );
+  }
+}
+
+if ($file) {
+  fclose($file);
+}
+
+if ($newf) {
+  fclose($newf);
+}
+
+
+if (file_exists($newfname)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($url).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($newfname));
+    readfile($newfname);
+    exit;
+}
+
+unlink($newfname);
+
+?>
